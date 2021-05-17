7e00ba9952478644f182995542dd26d92d1e502b
shovancak <s.hovancak@gmail.com>
2021-05-17 20:54:50 +0200

    Add finished exercises for Query Types.

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,21 @@
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    pets: (_, __, { models }) => {
+      return models.Pet.findMany()
     }
   },
-  User: {
+  // Mutation: {
     
-  }
+  // },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   }
+  // },
+  // User: {
+    
+  // }
 }
